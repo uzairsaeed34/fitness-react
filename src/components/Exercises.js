ecb74c541a981347f4bea0f3a48c1e26d7782ad5
uzairsaeed34 <uzairsaeed34k@gmail.com>
2024-08-04 00:55:35 +0500

    Fixed ESLint errors in Exercises.js and HeroBanner.js

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -36,7 +36,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
       setExercises(exerciseData);
     };
     fetcjExerciseData();
-  }, [bodyPart]);
+  }, [bodyPart, setExercises]);
   return (
     <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
       <Typography variant="h3" mb="46px">
